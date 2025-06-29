a86b10ebed52df09b875ba7fb6d4b7052af937ed
Akram Moumen El Idrissi <a.elidrissi@void.fr>
2025-06-29 10:16:44 +0100

    Use case

--- src/main/java/com/skypay/Main.java
+++ src/main/java/com/skypay/Main.java
@@ -1,8 +1,16 @@
 package com.skypay;
 
 
+import com.skypay.manager.AccountManager;
+
 public class Main {
     public static void main(String[] args) {
 
+        Account account = new Account(new AccountManager());
+
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
+        account.printStatement();
     }
 }
